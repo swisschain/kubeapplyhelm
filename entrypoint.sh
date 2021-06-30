ad5375b3c36e1831a2acc71bed0bd61d239e3701
Dvin <dvyu@yandex.ru>
2021-06-30 15:55:45 +0500

    helm

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@
 set -e
 
 # keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+#trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+#trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
 echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
