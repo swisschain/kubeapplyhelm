555efaecc960526605fd478f327354a9bf90ab08
Dvin <dvyu@yandex.ru>
2021-06-30 15:53:27 +0500

    helm

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,24 @@
 #!/bin/sh
 
 # exit when any command fails
-#set -e
+set -e
 
-echo hello!!!
 # keep track of the last executed command
-#trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-#trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
-#echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
-#export KUBECONFIG=/tmp/config
+echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
+export KUBECONFIG=/tmp/config
 
-#Configmap=$(find . -name "configmap.helm")
+Configmap=$(find . -name "configmap.helm")
 
-#for configmap in $Configmap
-#do
-#    echo helm apply $configmap
-#    cat $configmap
-#    helm upgrade -f $configmap
-#done
+for configmap in $Configmap
+do
+    echo helm apply $configmap
+    cat $configmap
+    helm upgrade -f $configmap
+done
 
-#rm /tmp/config
+rm /tmp/config
